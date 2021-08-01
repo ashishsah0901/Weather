3b3ff3f4fafed9d5358b952609f1916250b6b611
ashishsah0901 <69725687+ashishsah0901@users.noreply.github.com>
2021-08-01 14:47:48 +0530

    Update Constants.kt

--- app/src/main/java/com/example/weather/util/Constants.kt
+++ app/src/main/java/com/example/weather/util/Constants.kt
@@ -1,6 +1,6 @@
 package com.example.weather.util
 
 object Constants {
-    const val API_KEY = "c67be8058356937004e46a49e2bb2871"
+    const val API_KEY = "YOUR-API-KEY"
     const val BASE_URL = "http://api.openweathermap.org/"
 }
